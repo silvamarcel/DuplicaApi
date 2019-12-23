ecba333ac16e90660d5d94edc03ca4bca1e4722c
Marcel Silva <silva.marcel@gmail.com>
2019-12-23 16:23:07 +1100

    feat(acl): add acl rules and configuration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const acl = require('express-acl');
 
 const api = require('./api');
 const { auth, authRoutes } = require('./auth');
 
+const aclOptions = {
+  filename: 'acl.json',
+  path: 'server/config',
+  baseUrl: 'api',
+  decodedObjectName: 'user',
+};
+
 const server = ({ middleware }) => {
   const app = express();
+
+  // Setup of the Access Control List configuration
+  acl.config(aclOptions);
+  app.use(acl.authorize);
 
   // Setup the app middleware's
   middleware.appMiddleware(app);
