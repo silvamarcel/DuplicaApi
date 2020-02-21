f4efd8a90d0086ad6909c84d228759eec439ebfe
Marcel Silva <silva.marcel@gmail.com>
2020-02-21 13:03:10 +1100

    refactor(config): improves configuration file

--- server/config/config.js
+++ server/config/config.js
@@ -2,32 +2,35 @@ const _ = require('lodash');
 
 let config = {};
 
+const TEN_DAYS_IN_MINUTES = 24 * 60 * 10;
+
+const loadEnvironmentConfigurations = () => {
+  try {
+    return require(`./${config.env}`);
+  } catch (e) {
+    return {};
+  }
+};
+
 const init = () => {
   const DEV = 'dev';
+
   _.assign(config, {
     dev: 'development',
     test: 'testing',
     stage: 'staging',
     prod: 'production',
     port: process.env.PORT || 3000,
-    // 10 days in minutes
-    expireTime: process.env.EXPIRE_TIME || 24 * 60 * 10,
+    expireTime: process.env.EXPIRE_TIME || TEN_DAYS_IN_MINUTES,
     secrets: {
       jwt: process.env.JWT,
     },
   });
-
-  // Check if NODE_ENV was setted
   process.env.NODE_ENV = process.env.NODE_ENV || DEV;
+
   config.env = config[process.env.NODE_ENV];
 
-  // Will load the configurations of the environment which is running.
-  let envConfig;
-  try {
-    envConfig = require(`./${config.env}`);
-  } catch (e) {
-    envConfig = {};
-  }
+  const envConfig = loadEnvironmentConfigurations();
   config = _.assign(config, envConfig);
 };
 
