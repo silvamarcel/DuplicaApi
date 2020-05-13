a4af31023ba483f6c7b9f76f18867af582212bbc
Marcel Silva <silva.marcel@gmail.com>
2020-05-13 23:01:58 +1000

    feat(factory): add new fields

--- server/api/factory/factoryModel.js
+++ server/api/factory/factoryModel.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const FactorySchema = new Schema({
+  businessId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contract: {
     type: String,
     required: true,
     unique: true,
