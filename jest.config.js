7701112154255fb587c440e742383c57c93f7d14
Marcel Silva <marcel.silva@service.nsw.gov.au>
2020-08-08 20:53:27 +1000

    feat(test): add jest config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: 'node',
+  unmockedModulePathPatterns: ['./node_modules/lodash'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  collectCoverageFrom: ['server/**/*.js', 'src/**/*.js'],
+  coveragePathIgnorePatterns: [
+    'server/log/logger.js',
+    'server/store/database.js',
+    'server/middleware/appMiddleware.js',
+  ],
+};
